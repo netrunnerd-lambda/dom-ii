ab5d4702c388f1e4b81a9979bacd76cef82101e1
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-04-12 22:26:11 -0400

    implemented mouseover, scroll and dblclick events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// Your code goes here
+const select = s => document.querySelector(s),
+      selectAll = s => document.querySelectorAll(s);
+
+const groot = select('html'),
+      header = select('header'),
+      footer = select('footer');
+
+const funBusRave = _ => {
+  let r = Math.floor(Math.random() * 255),
+      g = Math.floor(Math.random() * 255),
+      b = Math.floor(Math.random() * 255);
+
+ groot.style.backgroundColor = `rgb(${r}, ${g}, ${b})`; 
+ header.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+ footer.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+const ohNoes = _ => {
+  groot.style.display = 'none';
+}
+
+window.addEventListener('dblclick', ohNoes);
+window.addEventListener('mousemove', funBusRave);
+window.addEventListener('scroll', funBusRave);
