e9c7b6f4174ab95e6e0c9c270c695c28c66d7b11
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-04-16 17:52:42 -0400

    implemented keydown, load, resize and select

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ const select = s => document.querySelector(s),
 
 const groot = select('html'),
       header = select('header'),
-      footer = select('footer');
+      nav = select('.nav-container nav'),
+      navLinks = selectAll('.nav-container nav .nav-link'),
+      footer = select('footer p');
 
 const funBusRave = _ => {
   let r = Math.floor(Math.random() * 255),
@@ -17,9 +19,62 @@ const funBusRave = _ => {
 }
 
 const ohNoes = _ => {
-  groot.style.display = 'none';
+  window.removeEventListener('mousemove', funBusRave);
+  window.removeEventListener('resize', funBusRave);
+  window.removeEventListener('scroll', funBusRave);
+
+  groot.style.backgroundColor = '';
+  groot.style.color = '';
+
+  header.style.backgroundColor = '';
+  footer.style.backgroundColor = '';
 }
 
 window.addEventListener('dblclick', ohNoes);
+window.addEventListener('keydown', ohNoes);
+
+window.addEventListener('load', _ => {
+  navLinks.forEach(link => {
+    const r = Math.floor(Math.random() * 255),
+          g = Math.floor(Math.random() * 255),
+          b = Math.floor(Math.random() * 255);
+
+    link.style.color = `rgb(${r}, ${g}, ${b})`;
+    link.style.fontWeight = 'bold';
+
+    setTimeout(_ => {
+      link.style.color = '';
+      link.style.fontWeight = '';
+    }, 1000);
+  });
+});
+
 window.addEventListener('mousemove', funBusRave);
+window.addEventListener('resize', funBusRave);
 window.addEventListener('scroll', funBusRave);
+
+header.addEventListener('click', _ => console.log('YEET'));
+
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log('AYE');
+  });
+
+  link.addEventListener('mouseover', _ => {
+    link.style.color = 'red';
+
+    setTimeout(_ => {
+      link.style.color = '';
+    }, 1000);
+  });
+});
+
+footer.appendChild(document.createElement('br'));
+footer.appendChild(document.createElement('textarea'));
+
+const footerText = select('footer textarea');
+
+footerText.addEventListener('select', e => console.log(e));
